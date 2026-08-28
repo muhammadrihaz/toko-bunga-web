79e2449c1565d367e93a21a1815eb8b3db7c5d4e
muhammadrihaz <MuhammadRihazjr9@gmail.com>
2026-08-28 15:51:48 +0800

    update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Admin Dashboard') - Fania Flower Shop</title>
+    <title>@yield('title', 'Admin - Fania Flower Shop')</title>
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('assets/logo.jpeg') }}" type="image/jpeg">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'Fania Flower Shop')</title>
+    
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('assets/logo.jpeg') }}" type="image/jpeg">
 
     <script src="https://cdn.tailwindcss.com"></script>
 
